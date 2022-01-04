dc87a55a58ef51ae5e693a71cc5d841cb4d222a5
Julian <julian.zubert@fh-bielefeld.de>
2022-01-04 16:56:37 +0100

    Added register routs

--- src/routes/register.py
+++ src/routes/register.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from flask import Blueprint, request, jsonify
 from data.user import User
-from data.usk import Usk
 
 from routes.user import User
 
@@ -9,17 +8,18 @@ blueprint = Blueprint("register", __name__)
 
 
 @blueprint.route("/register", methods=["POST"])
-
 def register():
-  user = User("Test", "Test@test.de", "IchHasseBrunsman",datetime.now)
-  if not request.is_json:
+
+    if not request.is_json:
         return "", 400
-  req = request.get_json()
-  if "username" in req:
-    user.set_username(req["username"])
-    
-  return """{
-  "password": "string",
-  "e_mail": "string",
-  "username": "string"
-}""", 200, {"Content-Type": "application/json"}
+    req = request.get_json()
+    if "username" in req and "password" in req and "e_mail" in req:
+        user = User(1, req["username"], req["e_mail"],
+                    req["password"], datetime.now())
+        #if database.create_user(user):
+        #    return "", 200
+        #else:
+        #    return "", 409
+        return "", 200
+    else:
+        return "", 400
