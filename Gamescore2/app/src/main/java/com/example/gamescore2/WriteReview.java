0feeb8d4014ffbe672f13f8850a8c7799e6716ed
Jason Piper <jasonpiper65@gmail.com>
2022-01-07 17:28:51 +0100

    cancel and confirm button function added, confirm intent = filler

--- Gamescore2/app/src/main/java/com/example/gamescore2/WriteReview.java
+++ Gamescore2/app/src/main/java/com/example/gamescore2/WriteReview.java
@@ -2,13 +2,31 @@ package com.example.gamescore2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class WriteReview extends AppCompatActivity {
+public class WriteReview extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_review);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.cancelButtonWriteReview:
+                Intent i = new Intent(this, Landingpage.class);
+                startActivity(i);
+                break;
+            case R.id.confirmButtonWriteReview:
+                Intent j = new Intent(this, Lists.class);
+                startActivity(j);
+                break;
+
+        }
+
     }
 }
