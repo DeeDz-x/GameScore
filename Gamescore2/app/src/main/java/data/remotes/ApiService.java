3c82aa3913a50b674d113c6cb1f52f66ac335a53
Marcel S <marcelschwarma@hotmail.de>
2022-01-10 11:29:26 +0100

    ApiService methods added

--- Gamescore2/app/src/main/java/data/remotes/ApiService.java
+++ Gamescore2/app/src/main/java/data/remotes/ApiService.java
@@ -18,6 +18,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 
 public interface ApiService {
 
@@ -32,5 +33,38 @@ public interface ApiService {
 
     @GET("/popular_games")
     Call<ResponseBody> getPopGames();
+
+    @POST("/forgot")
+    Call<ResponseBody> forgotPassword(@Header("Authorization") String token);
+
+    @POST("/update_password")
+    Call<ResponseBody> updatePassword(@Header("Authorization") String token, @Body RequestBody password);
+
+    @GET("/new_games")
+    Call<ResponseBody> getNewGames();
+
+    @GET("/games")
+    Call<ResponseBody> getGames();
+
+    @GET("/genre")
+    Call<ResponseBody> getGenre();
+
+    @GET("/publisher")
+    Call<ResponseBody> getPublisher();
+
+    @GET("/user")
+    Call<ResponseBody> getOwnProfile(@Header("Authorization") String token);
+
+    @PUT("/user")
+    Call<ResponseBody> updateOwnProfile(@Header("Authorization") String token, @Body RequestBody updatedProfile);
+
+    @PUT("/list")
+    Call<ResponseBody> createList (@Header("Authorization") String token, @Body RequestBody listName);
+
+    @PUT("/review")
+    Call<ResponseBody> createReview (@Header("Authorization") String token, @Body RequestBody reviewBody);
+
+    @GET("/time_played")
+    Call<ResponseBody> getTime(@Header("Authorization") String token);
 
 }
